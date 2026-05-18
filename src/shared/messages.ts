a778220dc9a32ff3e5738e8bb12565aa32bb53e7
Star <star.fullstack@gmail.com>
2026-05-18 15:27:06 -0400

    feat: upgrade to mv3-* libs v1.0.0 and restore ci

--- src/shared/messages.ts
+++ src/shared/messages.ts
@@ -1,5 +1,5 @@
-export interface Messages {
+export type Messages = {
 	GET_STATE: { input: void; output: { count: number; lastUpdated: number } };
 	INCREMENT: { input: { by: number }; output: { count: number } };
 	PING: { input: void; output: { pong: true; from: string } };
-}
+};
